b4160e5ca5048360c1d03935f38cbf4a660e4b07
Aziz Ahmed <azizahmed10088@gmail.com>
2025-04-25 17:11:40 +0500

    loader

--- app/(pages)/loading.jsx
+++ app/(pages)/loading.jsx
@@ -1,5 +1,11 @@
-const Loading = ({}) => {
-  return <div>loading...</div>
+const Loading = ({ }) => {
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '60vh' }}>
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    )
 }
 
 export default Loading
