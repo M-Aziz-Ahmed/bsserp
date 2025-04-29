d10126c097ff5185f66bd2d31ae295861fb6bbef
Aziz Ahmed <azizahmed10088@gmail.com>
2025-04-29 15:05:09 +0500

    ui

--- app/api/sendEmail/route.jsx
+++ app/api/sendEmail/route.jsx
@@ -1,40 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
-
 export async function POST(request) {
-  const { email, userName, message, services } = await request.json();
+    const { email, userName, message, services } = await request.json();
 
-  const transport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.MY_EMAIL,
-    to: process.env.MY_EMAIL,
-    // cc: 'info@bizsoftsolution.net',
-    subject: `Message from ${userName} (${email}) for ${services}`,
-    text: message,
-  };
-
-  const sendMailPromise = () =>
-    new Promise((resolve, reject) => {
-      transport.sendMail(mailOptions, function (err) {
-        if (!err) {
-          resolve('Email sent');
-        } else {
-          reject(err.message);
-        }
-      });
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MY_EMAIL,
+            pass: process.env.MY_PASSWORD,
+        },
     });
 
-  try {
-    await sendMailPromise();
-    return NextResponse.json({ message: 'Email sent' });
-  } catch (err) {
-    return NextResponse.json({ error: err }, { status: 500 });
-  }
+    try {
+        // Email to your company
+        await transport.sendMail({
+            from: process.env.MY_EMAIL,
+            to: process.env.MY_EMAIL,
+            subject: `Message from ${userName} (${email}) for ${services}`,
+            text: message,
+        });
+
+        // Auto-response to user
+        await transport.sendMail({
+            from: process.env.MY_EMAIL,
+            to: email,
+            subject: `We Hear You!`,
+            text: `Dear ${userName},\n\nThank you for reaching out to us. We have received your message and will get back to you shortly.\n\nBest regards,\nBizSoft Solution`,
+        });
+
+        return NextResponse.json({ message: 'Emails sent successfully' });
+    } catch (err) {
+        return NextResponse.json({ error: err.message }, { status: 500 });
+    }
 }
