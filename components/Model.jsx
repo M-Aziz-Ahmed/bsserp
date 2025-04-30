a3071a2eb6494c641d562957c1d53c9fc9f8830f
Aziz Ahmed <azizahmed10088@gmail.com>
2025-04-30 22:45:05 +0500

    responsive

--- components/Model.jsx
+++ components/Model.jsx
@@ -3,8 +3,8 @@ import Form from "./Form";
 const Model = ({ setModel, alert, setAlert }) => {
     return (
         <>
-            <div className="d-flex justify-content-center position-fixed py-5 w-100 z-3 bg-dark bg-opacity-50">
-                <div className="d-flex flex-column justify-content-center w-50">
+            <div className="d-flex justify-center md position-fixed py-5 w-100 z-3 bg-dark bg-opacity-50">
+                <div className="d-flex flex-column justify-content-center lg:w-150 md:w-150">
                     <div className="d-flex justify-content-end h2" onClick={() => { setModel(false) }} style={{ cursor: 'pointer' }}>X</div>
                     <Form  alert={alert} setAlert={setAlert}/>
                 </div>
