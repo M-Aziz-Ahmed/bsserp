9c6f9db3fd1b7562b9994dfd7011e4ff6de42e13
Aziz Ahmed <azizahmed10088@gmail.com>
2025-05-03 12:07:25 +0500

    ui

--- components/ClientStories.jsx
+++ components/ClientStories.jsx
@@ -50,11 +50,11 @@ const TrustedCustomers = () => {
                   </div>
                   <h3 className="h4 mb-3 text">{card.title}</h3>
                   <p className="text-secondary mb-4">{card.description}</p>
-                  <div className="">
+                  {/* <div className="">
                   <Link href={card.link} className="btn btn-theme btn-lg d-inline mt-auto rounded-5 px-3 py-2">
                     Read Case Study
                   </Link>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
